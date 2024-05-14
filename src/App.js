450c259b8cf18ea543e248103308db3b8b4543d3
Iffat2874 <iffatnizam28@gmail.com>
2024-05-14 21:07:22 +0530

    Project Completed

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import AboutUs from './components/AboutUs';
+
+function App() {
+  return (
+    <div>
+    <Home/>
+    <Gallery/>
+    <Contact/>
+    <AboutUs/>
+    </div>
+  );
+}
+
+export default App;
